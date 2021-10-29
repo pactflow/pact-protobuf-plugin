aaea71739ddcf9e8dfa5958f9d9f7d6c155bd9ba
Ronald Holshausen <ronald.holshausen@gmail.com>
2021-10-29 13:26:14 +1100

    chore: add initial configure interaction request implementation

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,6 @@
 //! Module provides the main gRPC server for the plugin process
 
-use log::debug;
+use log::{debug, error};
 use maplit::hashmap;
 use pact_plugin_driver::proto;
 use pact_plugin_driver::proto::catalogue_entry::EntryType;
@@ -48,7 +48,10 @@ impl PactPlugin for ProtobufPactPlugin {
     &self,
     _request: tonic::Request<proto::Catalogue>,
   ) -> Result<tonic::Response<()>, tonic::Status> {
-    unimplemented!()
+    debug!("Update catalogue request");
+
+    // currently a no-op
+    Ok(Response::new(()))
   }
 
   // Request to compare the contents and return the results of the comparison.
@@ -64,6 +67,124 @@ impl PactPlugin for ProtobufPactPlugin {
     &self,
     request: tonic::Request<proto::ConfigureInteractionRequest>,
   ) -> Result<tonic::Response<proto::ConfigureInteractionResponse>, tonic::Status> {
+    let message = request.get_ref();
+    debug!("Configure interaction request for content type '{}'", message.content_type);
+
+    // Check for the "pact:proto" key
+    let fields = message.contents_config.as_ref().map(|config| config.fields.clone()).unwrap_or_default();
+    if !fields.contains_key("pact:proto") {
+      error!("Config item with key 'pact:proto' and path to the proto file is required");
+      return Ok(Response::new(proto::ConfigureInteractionResponse {
+        error: "Config item with key 'pact:proto' and path to the proto file is required".to_string(),
+        .. proto::ConfigureInteractionResponse::default()
+      }))
+    }
+
+    // Check for either the message type or proto service
+    if !fields.contains_key("pact:message-type") && !fields.contains_key("pact:proto-service") {
+      let message = "Config item with key 'pact:message-type' and the protobuf message name or 'pact:proto-service' and the service name is required".to_string();
+      error!("{}", message);
+      return Ok(Response::new(proto::ConfigureInteractionResponse {
+        error: message,
+        .. proto::ConfigureInteractionResponse::default()
+      }))
+    }
+
+    /*
+    try {
+        val protoFile = Path.of(config["pact:proto"]!!.stringValue)
+        logger.debug { "Parsing proto file '$protoFile'" }
+        val protoResult = ProtoParser.parseProtoFile(protoFile)
+        val descriptorBytes = protoResult.toByteArray()
+        logger.debug { "Protobuf file descriptor set is ${descriptorBytes.size} bytes" }
+        val digest = MessageDigest.getInstance("MD5")
+        digest.update(descriptorBytes)
+        val descriptorHash = BaseEncoding.base16().lowerCase().encode(digest.digest());
+
+        logger.debug { "Parsed proto file OK, file descriptors = ${protoResult.fileList.map { it.name }}" }
+
+        val fileDescriptors = protoResult.fileList.associateBy { it.name }
+        val fileProtoDesc = fileDescriptors[protoFile.fileName.toString()]
+        if (fileProtoDesc == null) {
+          logger.error { "Did not find a file proto descriptor for $protoFile" }
+          return Plugin.ConfigureInteractionResponse.newBuilder()
+            .setError("Did not find a file proto descriptor for $protoFile")
+            .build()
+        }
+
+        if (logger.isTraceEnabled) {
+          logger.trace { "All message types in proto descriptor" }
+          for (messageType in fileProtoDesc.messageTypeList) {
+            logger.trace { messageType.toString() }
+          }
+        }
+
+        val interactions: MutableList<Plugin.InteractionResponse.Builder> = mutableListOf()
+
+        if (config.containsKey("pact:message-type")) {
+          val message = config["pact:message-type"]!!.stringValue
+          when (val result = configureProtobufMessage(message, config, fileProtoDesc, fileDescriptors, protoFile)) {
+            is Ok -> {
+              val builder = result.value
+              val pluginConfigurationBuilder = builder.pluginConfigurationBuilder
+              pluginConfigurationBuilder.interactionConfigurationBuilder
+                .putFields("message", Value.newBuilder().setStringValue(message).build())
+                .putFields("descriptorKey", Value.newBuilder().setStringValue(descriptorHash.toString()).build())
+              interactions.add(builder)
+            }
+            is Err -> {
+              return Plugin.ConfigureInteractionResponse.newBuilder()
+                .setError(result.error)
+                .build()
+            }
+          }
+        } else {
+          val serviceName = config["pact:proto-service"]!!.stringValue
+          when (val result = configureProtobufService(serviceName, config, fileProtoDesc, fileDescriptors, protoFile)) {
+            is Ok -> {
+              val (requestPart, responsePart) = result.value
+              val pluginConfigurationBuilder = requestPart.pluginConfigurationBuilder
+              pluginConfigurationBuilder.interactionConfigurationBuilder
+                .putFields("service", Value.newBuilder().setStringValue(serviceName).build())
+                .putFields("descriptorKey", Value.newBuilder().setStringValue(descriptorHash.toString()).build())
+              interactions.add(requestPart)
+              interactions.add(responsePart)
+            }
+            is Err -> {
+              return Plugin.ConfigureInteractionResponse.newBuilder()
+                .setError(result.error)
+                .build()
+            }
+          }
+        }
+
+        val builder = Plugin.ConfigureInteractionResponse.newBuilder()
+        val fileContents = protoFile.toFile().readText()
+        val valueBuilder = Value.newBuilder()
+        val structValueBuilder = valueBuilder.structValueBuilder
+        structValueBuilder
+          .putAllFields(
+            mapOf(
+              "protoFile" to Value.newBuilder().setStringValue(fileContents).build(),
+              "protoDescriptors" to Value.newBuilder()
+                .setStringValue(Base64.getEncoder().encodeToString(descriptorBytes))
+                .build()
+            )
+          )
+          .build()
+        val pluginConfigurationBuilder = builder.pluginConfigurationBuilder
+        pluginConfigurationBuilder.pactConfigurationBuilder.putAllFields(
+          mapOf(descriptorHash.toString() to valueBuilder.build())
+        )
+
+        for (result in interactions) {
+          logger.debug { "Adding interaction $result" }
+          builder.addInteraction(result)
+        }
+
+        return builder.build()
+     */
+
     unimplemented!()
   }
 
@@ -73,5 +194,75 @@ impl PactPlugin for ProtobufPactPlugin {
     request: tonic::Request<proto::GenerateContentRequest>,
   ) -> Result<tonic::Response<proto::GenerateContentResponse>, tonic::Status> {
     unimplemented!()
+  }
+}
+
+#[cfg(test)]
+#[allow(non_snake_case)]
+mod tests {
+  use expectest::prelude::*;
+  use maplit::btreemap;
+  use pact_plugin_driver::proto;
+  use pact_plugin_driver::proto::catalogue_entry::EntryType;
+  use pact_plugin_driver::proto::pact_plugin_server::PactPlugin;
+  use tonic::Request;
+
+  use crate::server::ProtobufPactPlugin;
+
+  #[tokio::test]
+  async fn init_plugin_test() {
+    let plugin = ProtobufPactPlugin {};
+    let request = proto::InitPluginRequest {
+      implementation: "test".to_string(),
+      version: "0".to_string()
+    };
+
+    let response = plugin.init_plugin(Request::new(request)).await.unwrap();
+    let response_message = response.get_ref();
+    expect!(response_message.catalogue.iter()).to(have_count(2));
+
+    let first = &response_message.catalogue.get(0).unwrap();
+    expect!(first.key.as_str()).to(be_equal_to("prototype"));
+    expect!(first.r#type).to(be_equal_to(EntryType::ContentMatcher as i32));
+    expect!(first.values.get("content-types")).to(be_some().value(&"application/protobuf".to_string()));
+
+    let second = &response_message.catalogue.get(1).unwrap();
+    expect!(second.key.as_str()).to(be_equal_to("prototype"));
+    expect!(second.r#type).to(be_equal_to(EntryType::ContentGenerator as i32));
+    expect!(second.values.get("content-types")).to(be_some().value(&"application/protobuf".to_string()));
+  }
+
+  #[tokio::test]
+  async fn configure_interaction_test__with_no_config() {
+    let plugin = ProtobufPactPlugin {};
+    let request = proto::ConfigureInteractionRequest {
+      content_type: "text/test".to_string(),
+      contents_config: Some(prost_types::Struct {
+        fields: btreemap!{}
+      })
+    };
+
+    let response = plugin.configure_interaction(Request::new(request)).await.unwrap();
+    let response_message = response.get_ref();
+    expect!(&response_message.error).to(
+      be_equal_to("Config item with key 'pact:proto' and path to the proto file is required"));
+  }
+
+  #[tokio::test]
+  async fn configure_interaction_test__with_missing_message_or_service_name() {
+    let plugin = ProtobufPactPlugin {};
+    let request = proto::ConfigureInteractionRequest {
+      content_type: "text/test".to_string(),
+      contents_config: Some(prost_types::Struct {
+        fields: btreemap!{
+          "pact:proto".to_string() => prost_types::Value { kind: Some(prost_types::value::Kind::StringValue("test.proto".to_string())) }
+        }
+      })
+    };
+
+    let response = plugin.configure_interaction(Request::new(request)).await.unwrap();
+    let response_message = response.get_ref();
+    expect!(&response_message.error).to(
+      be_equal_to("Config item with key 'pact:message-type' and the protobuf message name or 'pact:proto-service' and the service name is required"));
   }
 }
