6169c139e3ae5660f878991c7a855817eb19d5f9
Stan <stan@confluent.io>
2024-03-29 17:58:47 -0700

    sample map matching

--- integrated_tests/matching_maps/src/lib.rs
+++ integrated_tests/matching_maps/src/lib.rs
@@ -2,22 +2,22 @@ tonic::include_proto!("pactissue");
 
 #[cfg(test)]
 mod tests {
-  use std::path::Path;
+    use std::path::Path;
 
-  use expectest::prelude::*;
-  use maplit::hashmap;
-  use pact_consumer::prelude::*;
-  use pact_consumer::mock_server::StartMockServerAsync;
-  use serde_json::json;
+    use expectest::prelude::*;
+    use maplit::hashmap;
+    use pact_consumer::mock_server::StartMockServerAsync;
+    use pact_consumer::prelude::*;
+    use serde_json::json;
 
-  use super::*;
+    use super::*;
 
-  #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
-  async fn test_proto_client() {
-    let _ = env_logger::builder().is_test(true).try_init();
+    #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
+    async fn test_proto_client() {
+        let _ = env_logger::builder().is_test(true).try_init();
 
-    let mut pact_builder = PactBuilderAsync::new_v4("grpc-consumer-rust", "matching_maps");
-    let mock_server = pact_builder
+        let mut pact_builder = PactBuilderAsync::new_v4("grpc-consumer-rust", "matching_maps");
+        let mock_server = pact_builder
       .using_plugin("protobuf", None).await
       .synchronous_message_interaction("example request", |mut i| async move {
         let proto_file = Path::new("matching_maps.proto")
@@ -28,8 +28,11 @@ mod tests {
           "pact:proto-service": "ExampleService/GetSample",
 
           "request": {
-            "labels": {
-              "pact:match": "eachKey(matching(regex, '\\d+', '100')), eachValue(matching(regex, '(\\w|\\s)+', 'TestLabel'))"
+            "values": {
+              "pact:match": "eachKey(matching(regex, '\\d+', '100')), eachValue(matching($'example'))",
+              "example": {
+                "value": "matching(type, 'ABC')"
+              },
             },
             "ok": "true"
           },
@@ -43,18 +46,26 @@ mod tests {
       .start_mock_server_async(Some("protobuf/transport/grpc"))
       .await;
 
-    let url = mock_server.url();
-    let mut client = example_service_client::ExampleServiceClient::connect(url.to_string()).await.unwrap();
+        let url = mock_server.url();
+        let mut client = example_service_client::ExampleServiceClient::connect(url.to_string())
+            .await
+            .unwrap();
 
-    let request_message = ExampleRequest {
-      labels: hashmap! {
-        "12324".to_string() => "This is a label".to_string(),
-        "2233211".to_string() => "This is also a label".to_string()
-      },
-      ok: true
-    };
-    let response = client.get_sample(tonic::Request::new(request_message)).await;
-    let response_message = response.unwrap();
-    expect!(response_message.get_ref().ok).to(be_true());
-  }
+        let request_message = ExampleRequest {
+            values: hashmap! {
+              "12324".to_string() => ExampleValue{
+                value: "ABC".to_string(),
+              },
+              "2233211".to_string() => ExampleValue {
+                value: "XYZ".to_string()
+              }
+            },
+            ok: true,
+        };
+        let response = client
+            .get_sample(tonic::Request::new(request_message))
+            .await;
+        let response_message = response.unwrap();
+        expect!(response_message.get_ref().ok).to(be_true());
+    }
 }
