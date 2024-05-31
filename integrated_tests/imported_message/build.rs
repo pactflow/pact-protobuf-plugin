0fc95d2554a11afe0e85b07ea5e863ec4b29141e
Stan <stan@confluent.io>
2024-05-31 15:13:35 -0700

    conflicting names when package is implicit

--- integrated_tests/imported_message/build.rs
+++ integrated_tests/imported_message/build.rs
@@ -2,6 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure().include_file("mod.rs").compile(
         &[
             "primary/primary.proto",
+            "primary/rectangle.proto",
             "imported/imported.proto",
             "zimported/zimported.proto",
         ],
